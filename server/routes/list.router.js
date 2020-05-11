193287c12863dc557205e7420b3e3566d3a98f9d
demkantor <demkantor@gmail.com>
2020-05-11 16:20:01 -0500

    db routes created and tested working via postman

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -22,22 +22,92 @@ router.post('/', (req, res) => {
         title
     });
     newList.save().then((listDoc)=>{
-        console.table(listDoc);
+        console.log(listDoc);
         res.send(listDoc);
-    })
+    });
 });
 
 
 // update list with new values
 router.patch('/:id', (req, res)=>{
     console.log('in UPDATE list', req.body, req.params.id);
+    List.findOneAndUpdate({_id: req.params.id},  {
+        $set: req.body
+    }).then(()=>{
+        res.sendStatus(200);
+    });
 });
 
 
 // delete a list by id
 router.delete('/:id', (req, res)=>{
     console.log('in DELETE list', req.body, req.params.id);
+    List.findOneAndRemove({
+        _id: req.params.id
+    }).then((removeListDoc)=>{
+        res.send(removeListDoc);
+    });
 });
+
+//  get all tasks to a specific list
+router.get('/:listId/tasks', (req, res)=>{
+    console.log('in GET all tasks:',  req.params.listId);
+    Task.find({
+        _listId: req.params.listId
+    }).then((tasks)=>{
+        res.send(tasks);
+    });
+});
+
+//  get specific task from specific list by id
+router.get('/:listId/tasks/:taskId', (req, res)=>{
+    console.log('in GET specific task:', req.params.taskId, req.params.listId);
+    Task.findOne({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((task)=>{
+        res.send(task);
+    });
+});
+
+// add a new task to a specific list
+router.post('/:listId/tasks', (req, res) => {
+    console.log('in POST new task', req.body.title, req.params.listId);
+    let newTask= new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    });
+    newTask.save().then((newTaskDoc)=>{
+        console.log(newTaskDoc);
+        res.send(newTaskDoc);
+    });
+});
+
+// update task to a specific list by id
+router.patch('/:listId/tasks/:taskId', (req, res)=>{
+    console.log('in UPDATE task', req.body, req.params.taskId, req.params.listId);
+    Task.findOneAndUpdate({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    },  {
+        $set: req.body
+    }).then(()=>{
+        res.sendStatus(200);
+    });
+});
+
+
+// delete task to a specific list by id
+router.delete('/:listId/tasks/:taskId', (req, res)=>{
+    console.log('in DELETE task', req.body, req.params.taskId, req.params.listId);
+    Task.findOneAndRemove({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((removeTaskDoc)=>{
+        res.send(removeTaskDoc);
+    });
+});
+
 
 
 module.exports = router;
